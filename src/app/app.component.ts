0ef10d04b58794d46851ad79d4995d3c55787cc8
Edward <you@email.com>
2017-03-05 15:35:49 -0800

    deleted top menu, linked firelogo and added spin animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  Mars Colony
-  <a routerLink="/register">Register page</a>
-  <a routerLink="/encounters">Encounters page</a>
-  <a routerLink="/report">Report page</a>
-  <a routerLink="/notfound">Not found page</a>
   <div class="page">
     <router-outlet></router-outlet>
   </div>`,
